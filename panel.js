953610ad5ad4fdbc5b4bab1f9854f02dad55669a
Morgan Evans <moevans@adobe.com>
2017-10-11 17:02:37 -0700

    merge

---
+++ panel.js
@@ -0,0 +1,126 @@
+function toggleAccountPanelDark() {
+    var x = document.getElementById("accounts-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleAlbumPanelDark() {
+    var x = document.getElementById("albums-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleMembersPanelDark() {
+    var x = document.getElementById("members-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleFilterPanelDark() {
+    var x = document.getElementById("filters-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleSettingsPanelDark() {
+    var x = document.getElementById("settings-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleNotificationPanelDark() {
+    var x = document.getElementById("notifications-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleSharePanelDark() {
+    var x = document.getElementById("share-panel-dark");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+
+function toggleAccountPanelLight() {
+    var x = document.getElementById("accounts-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleAlbumPanelLight() {
+    var x = document.getElementById("albums-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleMembersPanelLight() {
+    var x = document.getElementById("members-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleFilterPanelLight() {
+    var x = document.getElementById("filters-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleSettingsPanelLight() {
+    var x = document.getElementById("settings-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleNotificationPanelLight() {
+    var x = document.getElementById("notifications-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+function toggleSharePanelLight() {
+    var x = document.getElementById("share-panel-light");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
